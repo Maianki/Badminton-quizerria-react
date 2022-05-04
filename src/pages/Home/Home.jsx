5f74419bde7e400453f90832b741f89d5cf6fa3e
Ankit Kumain <kumankit10@gmail.com>
2022-05-04 19:42:14 +0530

    fix: add error log

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,7 +20,7 @@ export function Home() {
         }));
         setCategories(allCategories);
       } catch (err) {
-        console.log(err);
+        console.error(err);
       }
     })();
   }, []);
