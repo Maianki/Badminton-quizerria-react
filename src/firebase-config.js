bf6452059fdd22a71e1c02df936b9de4a539c33a
Ankit Kumain <kumankit10@gmail.com>
2022-05-04 19:28:08 +0530

    fix: removed unwanted log from code and added API keys to .env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,13 +7,13 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBL-6fpd-kuJMDIrJJicLDQCSfErywxJMI",
-  authDomain: "badminton-quizerria.firebaseapp.com",
-  projectId: "badminton-quizerria",
-  storageBucket: "badminton-quizerria.appspot.com",
-  messagingSenderId: "48873582359",
-  appId: "1:48873582359:web:1a1802cc60e350a29ed152",
-  measurementId: "G-QX2JRY8NMZ",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,7 +25,6 @@ export function Home() {
     })();
   }, []);
 
-  console.log(categories);
   return (
     <div>
       <header>
