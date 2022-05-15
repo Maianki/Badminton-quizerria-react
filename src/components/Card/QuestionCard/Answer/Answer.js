71f424155db4b34f5e4390e4775379c47530cd07
Ankit Kumain <kumankit10@gmail.com>
2022-05-15 23:12:55 +0530

    fix: added changes suggested in review

--- src/components/Card/QuestionCard/Answer/Answer.js
+++ src/components/Card/QuestionCard/Answer/Answer.js
@@ -3,8 +3,6 @@ import { useGlobal } from "context/global-context";
 
 export default function Answer({ option, answer }) {
   const { globalDisptacher } = useGlobal();
-  //   const correctAnswerClass = answer === option ? `correct-answer` : ``;
-  //   const inCorrectAnswerClass = answer !== option ? `incorrect-answer` : ``;
   const handleAnswer = (option) => {
     globalDisptacher({ type: "SET_ANSWERS", payload: option });
   };
